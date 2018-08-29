58fb74e76caf9e6adb60f9a5caf8c18c3cc98523
Carbyn <carbyn.wu@gmail.com>
2018-08-29 16:31:20 +0800

    Add ango for review

---
+++ application/controllers/Ango.php
@@ -0,0 +1,15 @@
+<?php
+class AngoController extends \Explorer\ControllerAbstract {
+
+    const VERSION_IN_REVIEW = '1.0.79';
+
+    public function ahaAction() {
+        $ver = $this->getRequest()->getQuery('bhb');
+        $aha = false;
+        if ($ver === self::VERSION_IN_REVIEW) {
+            $aha = true;
+        }
+        return $this->outputSuccess(compact('aha'));
+    }
+
+}
