9f9cf8be32b0ffc1331c9e7fd158c072494d1680
Carbyn <carbyn.wu@gmail.com>
2018-08-31 00:51:16 +0800

    Publish new version

--- application/controllers/Ango.php
+++ application/controllers/Ango.php
@@ -1,7 +1,7 @@
 <?php
 class AngoController extends \Explorer\ControllerAbstract {
 
-    const VERSION_IN_REVIEW = '1.0.0';
+    const VERSION_IN_REVIEW = '1.0.86';
 
     public function ahaAction() {
         $ver = $this->getRequest()->getQuery('bhb');
