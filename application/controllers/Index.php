03ad50214270afcf0ab5e20a628d88545a2445f5
Carbyn <carbyn.wu@gmail.com>
2018-08-30 15:49:55 +0800

    Add official website

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -2,12 +2,7 @@
 class IndexController extends \Explorer\ControllerAbstract{
 
 	public function indexAction() {
-        exit;
-        $redis = new \Predis\Client();
-        for ($i = 1; $i < 6; $i++) {
-            $redis->del(md5('login_token_'.$i));
-        }
-        echo "hello";
+        $this->getView()->display('index/index.phtml');
 	}
 
 }
