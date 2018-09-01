d5a8eb24e7177a9717e78c52975732fe288626fd
Carbyn <carbyn.wu@gmail.com>
2018-09-01 23:30:50 +0800

    Add notice

--- application/controllers/Ango.php
+++ application/controllers/Ango.php
@@ -1,7 +1,7 @@
 <?php
 class AngoController extends \Explorer\ControllerAbstract {
 
-    const VERSION_IN_REVIEW = '1.0.88';
+    const VERSION_IN_REVIEW = '1.0.92';
 
     public function ahaAction() {
         $ver = $this->getRequest()->getQuery('bhb');
@@ -9,7 +9,10 @@ class AngoController extends \Explorer\ControllerAbstract {
         if ($ver === self::VERSION_IN_REVIEW) {
             $aha = true;
         }
-        return $this->outputSuccess(compact('aha'));
+        $notice = '“小猪钱吧”是一个能帮你用手机赚零花钱的小助手，按照引导完成任务，得到奖励即可提现。';
+        $new = 'wx69410165e46aab0b';
+        $new = '';
+        return $this->outputSuccess(compact('aha', 'notice', 'new'));
     }
 
 }
