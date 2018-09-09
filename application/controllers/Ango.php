7e51edf21324565539fb6332a426d86b24ccaee1
Carbyn <carbyn.wu@gmail.com>
2018-09-09 14:17:16 +0800

    Review done

--- application/controllers/Ango.php
+++ application/controllers/Ango.php
@@ -1,7 +1,7 @@
 <?php
 class AngoController extends \Explorer\ControllerAbstract {
 
-    const VERSION_IN_REVIEW = '1.0.107';
+    const VERSION_IN_REVIEW = '1.0.100';
 
     public function ahaAction() {
         $ver = $this->getRequest()->getQuery('bhb');
