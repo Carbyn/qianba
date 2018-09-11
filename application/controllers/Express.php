3578feed58ee78e0d7b5f2bed00677946abc3154
Carbyn <carbyn.wu@gmail.com>
2018-09-11 23:21:38 +0800

    Add express/shippers api

--- application/controllers/Express.php
+++ application/controllers/Express.php
@@ -12,39 +12,141 @@ class ExpressController extends \Explorer\ControllerAbstract {
             return $this->outputError(Constants::ERR_EXPRESS_NO_SHIPPERS, '单号无效，请确认后重新输入');
         }
         $shipper = $shippers['Shippers'][0];
-        $shipper['ShipperLogo'] = $this->getLogo($shipper['ShipperCode']);
+        $shippers = $this->getShippers();
+        if (isset($shippers[$shipper['ShipperCode']])) {
+            $shipper = $shippers[$shipper['ShipperCode']];
+        } else {
+            $shipper['ShipperLogo'] = 'https://qianba.1024.pm/static/youzi.png';
+            $shipper['ShipperPhone'] = '';
+        }
         $traces = \Explorer\Kdniao::search($shipper['ShipperCode'], $code);
         $traces = @json_decode($traces, true);
-        if (!$traces) {
-            return $this->outputError(Constants::ERR_EXPRESS_NO_TRACE, '暂时没有更多信息');
-        }
         $traces = array_reverse($traces['Traces']);
-        $this->outputSuccess(compact('shipper', 'traces'));
+        $this->outputSuccess(compact('code', 'shipper', 'traces'));
     }
 
-    private function getLogo($ShipperCode) {
-        $logos = [
-            'SF'        => 'https://qianba.1024.pm/static/express/shunfeng.png', //顺丰速运    SF
-            'HTKY'      => 'https://qianba.1024.pm/static/express/huitongkuaidi.png', //百世快递    HTKY
-            'ZTO'       => 'https://qianba.1024.pm/static/express/zhongtong.png', //中通快递    ZTO
-            'STO'       => 'https://qianba.1024.pm/static/express/shentong.png', //申通快递    STO
-            'YTO'       => 'https://qianba.1024.pm/static/express/yuantong.png', //圆通速递    YTO
-            'YD'        => 'https://qianba.1024.pm/static/express/yunda.png', //韵达速递    YD
-            'YZPY'      => 'https://qianba.1024.pm/static/express/youzhengguonei.png', //邮政快递包裹    YZPY
-            'EMS'       => 'https://qianba.1024.pm/static/express/ems.png', //EMS    EMS
-            'HHTT'      => 'https://qianba.1024.pm/static/express/tiantian.png', //天天快递    HHTT
-            'JD'        => 'https://qianba.1024.pm/static/express/jd.png', //京东快递    JD
-            'UC'        => 'https://qianba.1024.pm/static/express/youshuwuliu.png', //优速快递    UC
-            'DBL'       => 'https://qianba.1024.pm/static/express/debangwuliu.png', //德邦快递    DBL
-            'ZJS'       => 'https://qianba.1024.pm/static/express/zhaijisong.png', //宅急送    ZJS
-            'TNT'       => 'https://qianba.1024.pm/static/express/tnt.png', //TNT快递    TNT
-            'UPS'       => 'https://qianba.1024.pm/static/express/ups.png', //UPS    UPS
-            'DHL'       => 'https://qianba.1024.pm/static/express/dhl.png', //DHL    DHL
-            'FEDEX'     => 'https://qianba.1024.pm/static/express/fedex.png', //FEDEX联邦(国内件）    FEDEX
-            'FEDEX_GJ'  => 'https://qianba.1024.pm/static/express/fedex.png', //FEDEX联邦(国际件）    FEDEX_GJ
+    public function shippersAction() {
+        $shippers = array_values($this->getShippers());
+        $this->outputSuccess(compact('shippers'));
+    }
+
+    private function getShipper($ShipperCode) {
+        $shippers = $this->getShippers();
+
+    }
+
+    private function getShippers() {
+        $shippers = [
+            'SF' => [
+                'ShipperCode' => 'SF',
+                'ShipperName' => '顺丰速运',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/shunfeng.png',
+                'ShipperPhone' => '95338',
+            ],
+            'ZTO' => [
+                'ShipperCode' => 'ZTO',
+                'ShipperName' => '中通快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/zhongtong.png',
+                'ShipperPhone' => '95311',
+            ],
+            'STO' => [
+                'ShipperCode' => 'STO',
+                'ShipperName' => '申通快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/shentong.png',
+                'ShipperPhone' => '95543',
+            ],
+            'YTO' => [
+                'ShipperCode' => 'YTO',
+                'ShipperName' => '圆通快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/yuantong.png',
+                'ShipperPhone' => '95554',
+            ],
+            'EMS' => [
+                'ShipperCode' => 'EMS',
+                'ShipperName' => 'EMS',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/ems.png',
+                'ShipperPhone' => '11183',
+            ],
+            'YZPY' => [
+                'ShipperCode' => 'YZPY',
+                'ShipperName' => '邮政包裹',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/youzhengguonei.png',
+                'ShipperPhone' => '11183',
+            ],
+            'HTKY' => [
+                'ShipperCode' => 'HTKY',
+                'ShipperName' => '百世快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/huitongkuaidi.png',
+                'ShipperPhone' => '95320',
+            ],
+            'YD' => [
+                'ShipperCode' => 'YD',
+                'ShipperName' => '韵达快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/yunda.png',
+                'ShipperPhone' => '95546',
+            ],
+            'HHTT' => [
+                'ShipperCode' => 'HHTT',
+                'ShipperName' => '天天快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/tiantian.png',
+                'ShipperPhone' => '4001-888-888',
+            ],
+            'JD' => [
+                'ShipperCode' => 'JD',
+                'ShipperName' => '京东快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/jd.png',
+                'ShipperPhone' => '950616',
+            ],
+            'UC' => [
+                'ShipperCode' => 'UC',
+                'ShipperName' => '优速快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/youshuwuliu.png',
+                'ShipperPhone' => '400-1111-119',
+            ],
+            'DBL' => [
+                'ShipperCode' => 'DBL',
+                'ShipperName' => '德邦快递',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/debangwuliu.png',
+                'ShipperPhone' => '95353',
+            ],
+            'ZJS' => [
+                'ShipperCode' => 'ZJS',
+                'ShipperName' => '宅急送',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/zhaijisong.png',
+                'ShipperPhone' => '400-6789-000',
+            ],
+            'TNT' => [
+                'ShipperCode' => 'TNT',
+                'ShipperName' => 'TNT',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/tnt.png',
+                'ShipperPhone' => '800-820-9868',
+            ],
+            'UPS' => [
+                'ShipperCode' => 'UPS',
+                'ShipperName' => 'UPS',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/ups.png',
+                'ShipperPhone' => '400-820-8388',
+            ],
+            'DHL' => [
+                'ShipperCode' => 'DHL',
+                'ShipperName' => 'DHL',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/dhl.png',
+                'ShipperPhone' => '95380',
+            ],
+            'FEDEX' => [
+                'ShipperCode' => 'FEDEX',
+                'ShipperName' => 'FedEx',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/fedex.png',
+                'ShipperPhone' => '400-886-1888',
+            ],
+            'FEDEX_GJ' => [
+                'ShipperCode' => 'FEDEX_GJ',
+                'ShipperName' => 'FedEx国际',
+                'ShipperLogo' => 'https://qianba.1024.pm/static/express/fedex.png',
+                'ShipperPhone' => '400-886-1888',
+            ],
         ];
-        // todo
-        return isset($logos[$ShipperCode]) ? $logos[$ShipperCode] : 'https://qianba.1024.pm/static/express/default.png';
+        return $shippers;
     }
 
 }
