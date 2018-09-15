7d24c42fde2f5b0aea5a0e88b1ecaa31b4077f66
Carbyn <carbyn.wu@gmail.com>
2018-09-15 11:22:02 +0800

    Add new games

--- application/controllers/Next.php
+++ application/controllers/Next.php
@@ -3,84 +3,14 @@ class NextController extends \Explorer\ControllerAbstract {
 
     private $stat = 'from=youziyouxihezi';
 
-    public function indexAction() {
-        $matrix = [
-            [
-                'logo' => 'https://qianba.1024.pm/static/youzi.png',
-                'url' => 'wx7104ccdc4d907073',
-                'title' => '挑壁纸',
-            ],
-            [
-                'logo' => 'https://qianba.1024.pm/static/youzi.png',
-                'url' => 'wx618f3fe1bedd112e',
-                'title' => '个税计算',
-            ],
-            [
-                'logo' => 'https://qianba.1024.pm/static/youzi.png',
-                'url' => 'wx337881826fe875fc',
-                'title' => '快递助手',
-            ],
-        ];
-
-        $titles = [
-            'banner' => 'banner',
-            'recommend' => '热门推荐',
-            'partner' => '柚友必备',
-            'try' => '试玩专区',
-        ];
-        $duration = 60;
-        $stat = 'from=youziyouxihezi';
-
-        $bannerIds = [75, 68, 40];
-        $recommendIds = [52, 67, 51, 61, 35, 50, 59, 69, 64, 63, 48, 39];
-        $tryIds = [31, 32, 33, 34, 36, 37, 38, 46, 47, 49, 60, 62, 65, 66, 77, 71, 72, 73, 74, 76];
-
-        $taskModel = new TaskModel();
-        $tasks = $taskModel->batchFetch(array_merge($bannerIds, $recommendIds, $tryIds));
-        foreach($tasks as &$task) {
-            $task['stat'] = $stat;
-            $task['completed_num'] = 0;
-            $task['button_text'] = '马上玩';
-            if ($task['url']) {
-                $task['type'] = 'navigate';
-            } else {
-                $task['type'] = 'preview';
-            }
-        }
-
-        $banner = $recommend = $try = [];
-        foreach($bannerIds as $id) {
-            if (isset($tasks[$id])) {
-                $banner[] = $tasks[$id];
-            }
-        }
-        foreach($recommendIds as $id) {
-            if (isset($tasks[$id])) {
-                $recommend[] = $tasks[$id];
-            }
-        }
-        foreach($tryIds as $id) {
-            if (isset($tasks[$id])) {
-                $try[] = $tasks[$id];
-            }
-        }
-
-        shuffle($banner);
-        shuffle($recommend);
-        shuffle($try);
-
-        $games = compact('banner', 'recommend', 'matrix', 'try');
-
-        $this->outputSuccess(compact('titles', 'duration', 'games'));
-    }
+    private $bannerIds = [81, 82, 75, 40];
+    private $recommendIds = [83, 84, 85, 86, 87, 88, 52, 67, 51, 61, 35, 50];
+    private $matrixIds = [78, 79, 80];
+    private $feedIds = [59, 69, 64, 63, 48, 37, 31, 32, 33, 34, 36, 38, 39, 46, 47, 49, 60, 62, 65, 66, 77, 71, 72, 73, 74, 76];
 
     public function floorAction() {
-        $bannerIds = [81, 82, 75, 40];
-        $recommendIds = [52, 67, 51, 61, 35, 50, 59, 69, 64, 63, 48, 37];
-        $matrixIds = [78, 79, 80];
-
         $taskModel = new TaskModel();
-        $tasks = $taskModel->batchFetch(array_merge($bannerIds, $recommendIds, $matrixIds));
+        $tasks = $taskModel->batchFetch(array_merge($this->bannerIds, $this->recommendIds, $this->matrixIds));
         foreach($tasks as &$task) {
             $task['stat'] = $this->stat;
             $task['completed_num'] = 0;
@@ -93,17 +23,17 @@ class NextController extends \Explorer\ControllerAbstract {
         }
 
         $banner = $recommend = $try = [];
-        foreach($bannerIds as $id) {
+        foreach($this->bannerIds as $id) {
             if (isset($tasks[$id])) {
                 $banner[] = $tasks[$id];
             }
         }
-        foreach($recommendIds as $id) {
+        foreach($this->recommendIds as $id) {
             if (isset($tasks[$id])) {
                 $recommend[] = $tasks[$id];
             }
         }
-        foreach($matrixIds as $id) {
+        foreach($this->matrixIds as $id) {
             if (isset($tasks[$id])) {
                 $matrix[] = $tasks[$id];
             }
@@ -132,13 +62,12 @@ class NextController extends \Explorer\ControllerAbstract {
     }
 
     public function feedAction() {
-        $feedIds = [31, 32, 33, 34, 36, 38, 39, 46, 47, 49, 60, 62, 65, 66, 77, 71, 72, 73, 74, 76];
 
         $page = (int)$this->getRequest()->getQuery('page', 1);
         $page = max(1, $page);
         $limit = 25;
         $offset = ($page - 1) * $limit;
-        $feedIds = array_slice($feedIds, $offset, $limit);
+        $feedIds = array_slice($this->feedIds, $offset, $limit);
         if (empty($feedIds)) {
             $tasks = [];
         } else {
