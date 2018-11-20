fa92e6968686ee2419a9110b471703729647f2d8
Carbyn <carbyn.wu@gmail.com>
2018-11-20 21:26:34 +0800

    Add new game

--- application/controllers/Next.php
+++ application/controllers/Next.php
@@ -6,13 +6,13 @@ class NextController extends \Explorer\ControllerAbstract {
             'title' => '顶部横图',
             'category' => '',
             'componentName' => 'banner',
-            'ids' => [1254, 1255, 1155, 1012, 4, 8, 10, 14, 15]
+            'ids' => [1346, 1254, 1255, 1155, 1012, 4, 8, 10, 14, 15]
         ],
         'recommend' => [
             'title' => '热门推荐',
             'category' => '',
             'componentName' => 'site',
-            'ids' => [1254, 1255, 1155, 1012, 4, 7, 8, 10]
+            'ids' => [1346, 1254, 1255, 1155, 1012, 4, 7, 10]
         ],
         'matrix'    => [
             'title' => '柚友必备',
